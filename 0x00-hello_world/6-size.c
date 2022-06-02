92655161e56c5575ed7f9472b6490fd5ed9cb935
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-02 10:02:51 -0700

    A size of variables in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-int main()
+/**
+ * Thie is our mani
+ *  retuurn o
+ */
+int main(void)
 {
 	char charType;
 	int intType;
 	long int longintType;
 	long long int longlongintType;
 	float floatType;
+	
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
         printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
         printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-	return 0;
+	return (0);
 }
