819b3381529688e6270a568d00ba4e3c6cdbac7a
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-03 10:46:24 -0700

    A C program which print characters except p and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * * main-Entry point
+ * * Return: 0 if successful
+ */
+int main(void)
+{
+	char sm_let = 'a';
+	/* code started here*/
+	while (sm_let <= 'z')
+	{
+		if (sm_let != 'e' || sm_let != 'q')
+		sm_let++;
+		putchar(sm_let);
+		sm_let++;
+	}
+	putchar('\n');
+	return (0);
+}
