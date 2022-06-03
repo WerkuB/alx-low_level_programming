43d0277d1ec1dcd51f58e63ac25c9d40a856f466
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-03 09:20:22 -0700

    A C code which print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * * main-Entry point
+ * * Return: 0 if successful
+ */
+int main(void)
+{
+	char sm_let = 'a';
+	char ca_let = 'A';
+	/* code started here*/
+	while (sm_let <= 'z')
+	{
+		putchar(sm_let);
+		sm_let++;
+	}
+	while (ca_let <= 'Z')
+	{
+		putchar(ca_let);
+	}
+	putchar('\n');
+	return (0);
+}
