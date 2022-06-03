a10d38ed56e5dbd127269a069fefdd703e7abe42
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-03 13:40:48 -0700

    A C program which print combination of two number followd by comma and space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int num1, num2;
 	/* code started here*/
-	for (num1 = 0; num1 <= 8; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 1; num2 <= 9; num2++)
 		{
@@ -15,8 +15,12 @@ int main(void)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
-				putchar(',');
-				putchar(' ');
+				if (num1 != 8 || num2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		
 			}
 		}
 	}
