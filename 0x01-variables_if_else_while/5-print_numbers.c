33358b2227678852f5df9b41e505c0b87fdd489b
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-03 09:50:21 -0700

    A C program which print numbers followed by new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * * main-Entry point
+ * * Return: 0 if successful
+ */
+int main(void)
+{
+	int nums = 0;
+	/* code started here*/
+	while (nums <= 9)
+	{
+		printf("%d", nums);
+		nums++;
+	}
+	putchar('\n');
+	return (0);
+}
