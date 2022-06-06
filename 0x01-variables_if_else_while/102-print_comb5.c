5f174d3a9a9e663477ae295e44c574ef8f9154b5
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-06 06:20:05 -0700

    A C code which print combination of 5 0bjects

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,33 +5,24 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1, num2;
 	/* code started here*/
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 < 100 ; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = num1 + 1 ; num2 < 100; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
+			putchar (num1 / 10 + '0');
+			putchar (num1 % 10 + '0');
+			putchar (' ');
+			putchar (num2 / 10 + '0');
+			putchar (num2 % 10 + '0');
+			if (num1 == 98 && num2 == 99)
 			{
-				for (num4 = 1; num4 <= 9; num4++)
-				{
-					if (num2 < num4 && num2 < num3)
-					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(' ');
-						putchar(num3 + '0');
-						putchar(num4 + '0');
-						if (num1 <= 9 || num2 <= 8 || num3 <=9 || num4 <= 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
-	}	
-	putchar('\n');
+	}
 	return (0);
 }
