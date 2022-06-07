88c27dd7b787891335cb8c611db3236ba7edae7b
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-07 10:58:47 -0700

    A function which print english letters a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet- print lowercas letters a-z
+ * Return:nothing,return vidd
+ */
+	void print_alphabet(void)
+	{
+		char c;
+		/* loop which print a-z*/
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	}
