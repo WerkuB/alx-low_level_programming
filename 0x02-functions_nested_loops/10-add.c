b4e01d1546592d01d25951d31100ab968470b56a
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-07 13:17:45 -0700

    A C function which compute sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -add two integers
+ * Return: sum of two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
