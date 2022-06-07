f0b00b4aeb61bd1e5bc0cd1ce395315aa6d8a53e
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-07 11:24:50 -0700

    A C function which check the characte is lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if the characte is lowercase
+ * @c: integer to be checked by function
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
