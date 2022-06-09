c967fabb3fbb86f1660bc55f4d5e41f2faea718e
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-09 04:42:41 -0700

    A C code which prints backslash

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_diagonal - print backslash in the terminal.
+ *@n: print int
+ *Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+
+
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar (92);
+			_putchar ('\n');
+		}
+	}
+
+	else
+	{
+		_putchar ('\n');
+	}
+
+}
