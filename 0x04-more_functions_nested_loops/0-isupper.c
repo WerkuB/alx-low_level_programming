661ae67f3a8c5cbe5a609d82be0a61c9759f30fb
Werku Birhanu Amsal <workzman12@gmail.com>
2022-06-09 00:59:43 -0700

    Readme isupper function and main header

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - return 1 if c is upper otherwise 0
+ * @c: a character to be cheked by a function
+ * Return: 1 0
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
