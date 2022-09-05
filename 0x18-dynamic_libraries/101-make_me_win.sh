86fa9e32e5ee9b7a850dd43c94e1c251e69424f3
Werku Birhanu Amsal <workzman12@gmail.com>
2022-09-05 14:56:35 +0300

    Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o nrandom.so
-LD_PRELOAD=$PWD/nrandom.so
+wget -P /tmp https://github.com/WerkuB/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
